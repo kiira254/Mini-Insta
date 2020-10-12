2ee43bb7ba26a95419931b5af9f6a0adb3c18667
kiira254 <nkamotho57@gmail.com>
2020-10-12 21:36:25 +0300

    add images in settings

--- images/urls.py
+++ images/urls.py
@@ -3,6 +3,11 @@ from django.urls import path,include
 
 urlpatterns = [
     path('', views.post, name = 'post'),
-    
+    path('image/<int:id>',views.image,name ='image'),
+    path('new/image', views.new_article, name='new-image'),
+    path('search/', views.search_results, name='search_results'),
     
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
--- images/views.py
+++ images/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from django.http import Http404, HttpResponse
 # Create your views here.
 
 def post (request):
