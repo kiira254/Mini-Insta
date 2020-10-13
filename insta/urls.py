32514ffdaa773e91b502f96b51c577fe2d3c1bf5
kiira254 <nkamotho57@gmail.com>
2020-10-13 19:40:11 +0300

    add new image

--- insta/urls.py
+++ insta/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import logout
+from tinymce import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
